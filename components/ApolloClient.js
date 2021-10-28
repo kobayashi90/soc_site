32cf6530b02e758e4802d7300a6ab226a66583c6
jorgev259 <jorgev259@outlook.es>
2021-10-28 01:55:47 -0600

    [Server] add github check

--- components/ApolloClient.js
+++ components/ApolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 const isSSR = typeof window === 'undefined'
+const isGithub = process.env.NODE_ENV === 'GITHUB'
 
 const client = new ApolloClient({
-  uri: isSSR ? 'http://localhost:4000' : 'http://localhost:4000',
+  uri: isSSR ? (isGithub ? 'https://beta.sittingonclouds.net/api' : 'http://localhost:4000') : 'http://localhost:4000',
   cache: new InMemoryCache(),
   ssrMode: isSSR
 })
