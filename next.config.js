78aeb6e7994230cb5f9bd2e8606188752badde16
jorgev259 <jorgev259@outlook.es>
2021-10-28 01:12:19 -0600

    Change action scripts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   images: {
     domains: ['beta.sittingonclouds.net']
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
   }
 }
