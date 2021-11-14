1633646b94536f2c584f74cce76a24c5b9d5163c
jorgev259 <jorgev259@outlook.es>
2021-11-13 23:53:54 -0600

    Move config files to env variables

--- prepareCI.js
+++ prepareCI.js
@@ -1,7 +1,5 @@
-const glob = require('glob')
-const { writeJSONSync, readFileSync, appendFileSync, removeSync } = require('fs-extra')
+const { readFileSync, appendFileSync, removeSync } = require('fs-extra')
 const gitignore = readFileSync('./.gitignore', 'utf-8').replace('/.next/\n', '')
 
-glob.sync('./config/**.example.json').forEach(p => writeJSONSync(p.replace('.example', ''), {}))
 removeSync('./.gitignore')
 appendFileSync('./.gitignore', gitignore)
