8a4e539864d4cad677714a75221bc4f8b01ee7a5
jorgev259 <jorgev259@outlook.es>
2021-12-24 18:20:24 -0600

    change redirects to .com

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
   eslint: { ignoreDuringBuilds: true },
   async redirects () {
     return [
-      { source: '/anim/:id', destination: 'https://www.sittingonclouds.net/anim/:id', permanent: false },
-      { source: '/studio/:slug', destination: 'https://www.sittingonclouds.net/studio/:slug', permanent: false }
+      { source: '/anim/:id', destination: 'https://www.sittingonclouds.com/anim/:id', permanent: false },
+      { source: '/studio/:slug', destination: 'https://www.sittingonclouds.com/studio/:slug', permanent: false }
     ]
   },
 
