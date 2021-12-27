e046d9cbc74b4ba3a84ab0fe1c09f1c88cc687f5
jorgev259 <jorgev259@outlook.es>
2021-12-27 12:47:59 -0600

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const domainNames = ['net']
+const domains = []
+
+domainNames.forEach(d => domains.push(`sittingonclouds.${d}`, `www.sittingonclouds.${d}`))
+
 module.exports = {
   eslint: { ignoreDuringBuilds: true },
-  domains: ['sittingonclouds.net'],
+  domains,
   async redirects () {
     return [
       { source: '/anim/:id', destination: 'https://www.sittingonclouds.com/anim/:id', permanent: false },
