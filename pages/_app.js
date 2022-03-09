ec558b27868df2654d105bdfe86c5f7347b173b2
jorgev259 <jorgev259@outlook.es>
2022-03-08 22:12:42 -0600

    Remove ad test

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,6 @@ import 'react-toastify/dist/ReactToastify.css'
 
 import Head from 'next/head'
 import { Container } from 'react-bootstrap'
-import Script from 'next/script'
 import { ToastContainer } from 'react-toastify'
 import { useEffect, useRef } from 'react'
 import { ApolloProvider } from '@apollo/client'
@@ -46,7 +45,6 @@ export default function MyApp ({ Component, pageProps }) {
         <meta key='desc' property='og:description' content='Largest Video Game & Animation Soundtrack サウンドトラック Archive' />
         <meta key='image' property='og:image' content='/img/assets/clouds_thumb.png' />
       </Head>
-      <Script data-cfasync="false" src="//dba9ytko5p72r.cloudfront.net/?tyabd=951926"></Script>
       <ToastContainer newestOnTop />
       <SSRProvider>
         <Header />
