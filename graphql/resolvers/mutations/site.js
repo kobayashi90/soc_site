e9dfd9ff786bb63e8c9318660094c431e4125ec3
jorgev259 <jorgev259@outlook.es>
2022-03-23 23:55:14 -0600

    enable upload banner

--- graphql/resolvers/mutations/site.js
+++ graphql/resolvers/mutations/site.js
@@ -1,19 +1,25 @@
 
 // import { AuthenticationError } from 'apollo-server-micro'
 // const { jwtApi } = require('../../useJwt')
+
+import { composeResolvers } from '@graphql-tools/resolvers-composition'
+import { hasRole, img } from '@/lib/utils'
+
 // const { img } = require('../helpers')
 
-module.exports = {
+const resolversComposition = { 'Mutation.*': hasRole('UPDATE') }
+const resolvers = {
   Mutation: {
     /* config: async (parent, data, { db, payload }, info) => {
     // if (!await jwtApi(payload, db, ['UPDATE'])) throw new AuthenticationError()
       return db.models.config.upsert(data).then(() => db.models.config.findByPk(data.name))
-    },
+    }, */
 
     uploadBanner: async (parent, { banner }, { db, payload }) => {
-    // if (!await jwtApi(payload, db, ['UPDATE'])) throw new AuthenticationError()
-      await img(banner, '/var/www/soc_img/img/live', 'banner')
+      await img(banner, 'live', 'banner')
       return 1
-    } */
+    }
   }
 }
+
+export default composeResolvers(resolvers, resolversComposition)
