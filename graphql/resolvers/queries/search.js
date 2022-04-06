63ba29a7770a3a78f8f5cc40dcfa8efc174086f7
jorgev259 <jorgev259@outlook.es>
2022-04-05 18:45:43 -0600

    Split and rename some resolver files

--- graphql/resolvers/queries/search.js
+++ graphql/resolvers/queries/search.js
@@ -3,26 +3,6 @@ import info from '@/config/info.json'
 
 const resolvers = {
   Query: {
-    artists: (parent, args, { db }, info) => db.models.artist.findAll(),
-    platforms: (parent, args, { db }, info) => db.models.platform.findAll(),
-    publishers: (parent, args, { db }, info) => db.models.publisher.findAll(),
-    publisher: (parent, { id }, { db }, info) => db.models.publisher.findByPk(id),
-    classes: (parent, args, { db }, info) => db.models.class.findAll(),
-    series: (parent, args, { db }, info) => db.models.series.findAll(),
-    categories: (parent, args, { db }, info) => db.models.category.findAll(),
-    games: (parent, args, { db }, info) => db.models.game.findAll(),
-    game: (parent, { slug }, { db }, info) => db.models.game.findByPk(slug),
-
-    album: (parent, { id, title }, { db }, info) => db.models.ost.findByPk(id),
-    downloads: (parent, { id }, { db }) => db.models.download.findAll({ where: { ostId: id } }),
-    albums: (parent, args, { db }, info) => db.models.ost.findAll(),
-
-    platform: async (parent, { id }, { db }) => db.models.platform.findByPk(id),
-    animation: (parent, { id }, { db }) => db.models.animation.findByPk(id),
-    animations: (parent, args, { db }) => db.models.animation.findAll(),
-    studio: (parent, { slug }, { db }) => db.models.studio.findByPk(slug),
-    studios: (parent, { slug }, { db }) => db.models.studio.findAll(),
-    seriesOne: (parent, { slug }, { db }, info) => db.models.series.findByPk(slug),
     searchAlbum: (parent, { title = '', classes = info.classes, limit, page = 0, order = ['createdAt'], mode = 'DESC', status = ['show'] }, { db }) => {
       const titleWords = title.split(' ')
 
@@ -64,17 +44,12 @@ const resolvers = {
       where: { name: { [Op.like]: `%${name}%` } },
       order: [[order, mode]]
     }, db),
-    albumCount: async (parent, params, { db }) => db.models.ost.count(),
     searchSeriesByName: (parent, { name }, { db }) => db.models.series.findAll({
       where: {
         name: {
           [Op.like]: `%${name}%`
         }
       }
-    }),
-    recentSeries: (parent, { limit }, { db }) => db.models.series.findAll({
-      limit: limit,
-      order: [['createdAt', 'DESC']]
     }),
     searchPublishersByName: (parent, { name }, { db }) => db.models.publisher.findAll({
       where: {
@@ -83,10 +58,6 @@ const resolvers = {
         }
       }
     }),
-    recentPublishers: (parent, { limit }, { db }) => db.models.publisher.findAll({
-      limit: limit,
-      order: [['createdAt', 'DESC']]
-    }),
     searchPlatformsByName: (parent, { name, type }, { db }) => db.models.platform.findAll({
       where: {
         name: {
@@ -94,17 +65,7 @@ const resolvers = {
         },
         type: { [Op.like]: `%${type}%` }
       }
-    }),
-    recentPlatforms: (parent, { limit, type }, { db }) => db.models.platform.findAll({
-      limit: limit,
-      order: [['createdAt', 'DESC']],
-      where: { type: { [Op.like]: `%${type}%` } }
-    }),
-
-    getRandomAlbum: async (parent, { limit = 1 }, { db }) => {
-      const result = await db.models.ost.findAll({ order: db.random(), limit })
-      return result
-    }
+    })
   }
 }
 
