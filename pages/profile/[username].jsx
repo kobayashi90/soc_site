667b1b0672964662e9a3e946aa37c5d765533794
jorgev259 <jorgev259@outlook.es>
2022-05-16 15:47:43 -0600

    THe thing

--- pages/profile/[username].jsx
+++ pages/profile/[username].jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import { gql, useMutation } from '@apollo/client'
 import { Container, Col, Row, Modal, Form, Button } from 'react-bootstrap'
 import { DateTime } from 'luxon'
 import Image from 'next/image'
+import ConfettiExplosion from 'react-confetti-explosion'
 
 import { AlbumBoxList } from '@/components/AlbumBoxes'
 import { initializeApollo } from '@/lib/ApolloClient'
@@ -9,7 +11,6 @@ import CommentCarrousel from '@/components/CommentsCarrousel'
 import { ButtonLoader } from '@/components/Loader'
 import serialize from 'form-serialize'
 import { toast } from 'react-toastify'
-import { useState } from 'react'
 import useUser from '@/components/useUser'
 import Head from 'next/head'
 
@@ -50,6 +51,17 @@ export async function getServerSideProps (context) {
   return { props: { userProfile: user } }
 }
 
+function Explosion (props) {
+  const { username } = props
+  const [isExploding] = useState(username === 'ChitoWarlock')
+
+  return (
+    <div className='h-100 w-100' style={{ position: 'absolute', pointerEvents: 'none' }}>
+      {isExploding && <ConfettiExplosion floorHeight={2000} floorWidth={2000} particleCount={250} />}
+    </div>
+  )
+}
+
 export default function Profile (props) {
   const { user } = useUser()
   const { userProfile } = props
@@ -72,10 +84,12 @@ export default function Profile (props) {
       </Head>
       {user?.username === username && <EditProfile setProfile={setProfile} showProfile={showProfile} />}
       <Container>
+        <Explosion username={username} />
         <Row className='mt-3'>
           <Col xs='auto' className='blackblock'>
             <div className='p-1 position-relative' style={{ height: '200px', width: '200px' }}>
               <Image style={{ borderRadius: '25px' }} layout='fill' alt={'placeholder'} src={imgUrl} blurDataURL={placeholder} />
+              {username === 'ChitoWarlock' && <img className='position-absolute' src='/img/assets/hat.png' style={{ height: '150px', width: '150px', top: '-70px', left: '25px', transform: 'rotate(10deg)' }}/>}
             </div>
           </Col>
           <Col className='blackblock ms-3 my-0 d-flex justify-content-center flex-column'>
